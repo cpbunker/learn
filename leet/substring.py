421a14c56f845962875d2e3c8ecb4efd6394016b
Christian Bunker <christian@bunkerplanet.com>
2022-05-06 12:32:18 -0400

    leet code practice

---
+++ leet/substring.py
@@ -0,0 +1,67 @@
+'''
+https://github.com/cpbunker/learn/leet
+
+Leetcode practice
+'''
+
+import time
+import random
+
+def solution(s: str, method = 'brute') -> int:
+    '''
+    '''
+
+    if(method == 'brute'):
+        counter = 0; # holds the length of longest substring at given time
+
+        # iter over all start and end chars -> all possible substrings
+        for starti in range(len(s)):
+            for stopi in range(starti,len(s)):
+                sub = s[starti:stopi+1];
+                if(len(sub) > counter): # else don't bother
+                    # iter over substring for repition
+                    sub_good = True;
+                    for i in range(len(sub)):
+                        for j in range(len(sub)):
+                            if(i != j and sub[i] == sub[j]):
+                                sub_good = False; # there is repitition
+                    if(sub_good):
+                        counter = len(sub);
+
+        # return length of longest substring
+        return counter
+
+    if(method == 'fast'):
+        counter = 0; # stores length of longest substring at given time
+        
+        # special case
+        if(len(s) == 1):
+            return 1;
+        
+        # iter over all possible substrings
+        for starti in range(len(s)):
+            if(len(s) - starti > counter): # else don't bother
+                for stopi in range(starti+1,len(s)):
+
+                    # check for end of string or repition
+                    if(s[stopi] in s[starti:stopi]):
+                        counter = max(counter,stopi-starti);
+                        break;
+                    elif(stopi == len(s) - 1):
+                        counter = max(counter, len(s) - starti);
+                        break
+                        
+        return counter;
+
+#### test the soln
+mystr = "civilizationivbeyondthesword";
+start = time.time();
+sol = solution(mystr);
+stop = time.time();
+print("\nSolution:",sol);
+print("Time = ", stop - start);
+    
+
+
+
+
---
+++ leet/twosum.py
@@ -0,0 +1,61 @@
+'''
+https://github.com/cpbunker/learn/leet
+
+Leetcode practice
+'''
+
+import time
+import random
+
+def solution(nums: list[int], target: int, method = 'ifin') -> list[int]:
+    '''
+    takes a list of nums for which there is one pair s.t.
+    nums[i] + nums[j] == target, and returns indices i,j
+    '''
+
+    if(method == 'brute'):
+        for i1 in range(len(nums)):
+            for i2 in range(len(nums)):
+                if(nums[i1] + nums[i2] == target):
+                    return [i1, i2];
+
+    if(method == 'ifin'):
+        for i1 in range(len(nums)):
+            complement = target - nums[i1];
+            if complement in nums: # speedup 
+                for i2 in range(len(nums)):
+                    if(nums[i1] + nums[i2] == target):
+                        return [i1, i2];
+
+def screen(nums: list[int], target: int) -> bool:
+    '''
+    makes sure there is exactly one unique solution to above
+    '''
+
+    counter = 0;
+    for n1 in nums:
+        for n2 in nums:
+            if(n1 + n2 == target):
+                counter += 1;
+    return(counter == 2);
+
+
+#### test the soln
+mynums = list(range(-10000,-100));
+mynums.append(4)
+mynums.append(5);
+myt = 9
+
+# randomize mynums
+random.shuffle(mynums);
+if screen(mynums, myt):
+    start = time.time();
+    inds = solution(mynums, myt);
+    stop = time.time();
+    print("Time = ", stop - start);
+    assert(mynums[inds[0]]+mynums[inds[1]] == myt); 
+    
+
+
+
+
